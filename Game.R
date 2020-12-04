15144ce5377eb4145156f767e550b6220bb1f818
Jan Kroon <kroon@mac.com>
2020-12-04 17:41:27 +0100

    Added code to RandomRat.R file

--- Game.R
+++ Game.R
@@ -1,1 +1,9 @@
 # Game loop for multiple Blackjack player and the bank
+
+source("RandomRat.R")
+
+# Test Cases
+RandomRat.bankroll <- 50000
+
+print(paste("RandomRat bets: ", bet(RandomRat.bankroll)))
+print(paste("RandomRat turn: ", turn(0, 0)))
--- RandomRat.R
+++ RandomRat.R
@@ -1,1 +1,13 @@
 # Player without any strategy
+
+bet <- function(bankroll){
+  amount <- sample(1:bankroll, size=1)
+  bankroll <- bankroll - amount
+  return(amount)
+}
+
+turn <- function(bank.hand, my.hand){
+  response = sample(c("pass", "hit", "double", "split"), size=1)
+  return(response)
+}
+
